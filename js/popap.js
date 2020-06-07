023b4b5a3a9b170ad657f0ca23e7d7ec8f91cd3e
gigabot04 <64404363+gigabot04@users.noreply.github.com>
2020-06-07 15:06:14 +0300

    Сделал попап

---
+++ js/popap.js
@@ -0,0 +1,15 @@
+let fiew = document.querySelectorAll(".block");
+let btnClose = document.querySelector(".close");
+let btn = document.querySelector(".block-btn");
+
+btn.onclick = function () {
+  for (let i = 0; i < fiew.length; i++) {
+    fiew[i].style.display = "block";
+  }
+}
+
+btnClose.onclick = function () {
+  for (let i = 0; i < fiew.length; i++) {
+    fiew[i].style.display = "none";
+  }
+}
